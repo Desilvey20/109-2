64999b51fe54f1aac2c2a072b23ac4c3ffad841b
Desilvey20 <101611124+Desilvey20@users.noreply.github.com>
2022-06-08 17:37:25 -0700

    Add files via upload

---
+++ src/components/cart.jsx
@@ -0,0 +1,13 @@
+
+
+
+const Cart = () => {
+    return (
+        <div className="cart">
+            <h6>These are the products on your Cart</h6>
+            <h3>Are you ready to place the order ?</h3>
+        </div>
+    )
+}
+
+export default Cart;
